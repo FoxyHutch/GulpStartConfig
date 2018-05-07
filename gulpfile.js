d9a37aed363e834a2c5ddafd34086f6e6c74c44c
Kester <kester.lotz@gmail.com>
2018-05-07 19:15:52 +0200

    Add: Watcher for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+
 //Sass muss ebenfalls als dependency bekannt gemacht werden
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+
+//Autorefixer
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', defaultTask);
 
@@ -8,6 +12,10 @@ gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         // Konvertiert alle in CSS-Files und erzuegt Fehlermeldung bei Fehler ohne verarbeitung abzubrechen
         .pipe(sass().on('error', sass.logError))
+        // Autoprefixer über CSS jagen
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
         .pipe(gulp.dest('./css'));
 });
 
