20a589fdee544d2ccd9c553239078e4904cf8c60
Kester <kester.lotz@gmail.com>
2018-05-07 18:50:22 +0200

    Add SASS to gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+//Sass muss ebenfalls als dependency bekannt gemacht werden
+var sass = require('gulp-sass')
 
 gulp.task('default', defaultTask);
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        // Konvertiert alle in CSS-Files und erzuegt Fehlermeldung bei Fehler ohne verarbeitung abzubrechen
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
 
 function defaultTask(done) {
     console.log("First Gulp-Task")
