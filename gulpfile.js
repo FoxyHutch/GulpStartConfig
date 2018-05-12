de1e7bcc3a582e1e3af7f8a5f60dc91921afd7e1
Kester <kester.lotz@gmail.com>
2018-05-12 19:39:21 +0200

    feat: Add Image-Resizing

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,12 @@ var imagemin = require('gulp-imagemin');
 //PNG-Qunatization
 var pngquant = require('imagemin-pngquant');
 
+//Image Resize
+var imageresize = require('gulp-image-resize');
+
+//Rename
+var rename = require('gulp-rename');
+
 
 gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function() {
     gulp.watch('sass/**/*.scss', ['styles']);
@@ -37,7 +43,8 @@ gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function
     gulp.watch('./js/**/*.js', ['scripts']);
 
     browserSync.init({
-        server: './dist/'
+        server: './dist/',
+        browser: "google chrome"
     })
 });
 
@@ -48,17 +55,6 @@ gulp.task('dist', [
     'scripts-dist',
     'copy-images-dist'
 ]);
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        browser: "google chrome"
-    });
-    browserSync.stream();
-});
 
 gulp.task('scripts', function() {
     gulp.src('./js/**/*.js')
@@ -88,17 +84,72 @@ gulp.task('copy-images', function() {
         .pipe(gulp.dest('./dist/img'))
 })
 
-gulp.task('copy-images-dist', function() {
+
+//Überarbeitet und kopiert alle Images
+gulp.task('copy-images-dist', [
+    'copy-images-large',
+    'copy-images-medium',
+    'copy-images-small'
+])
+
+gulp.task('copy-images-large', function() {
     gulp.src('./img/*')
+        //Large
+        .pipe(imageresize({
+            width: 1920,
+            upscale: false,
+            crop: false,
+            imageMagick: true
+        }))
         .pipe(imagemin({
             progressive: true,
             use: [pngquant()]
+        }))
+        .pipe(rename({
+            suffix: '_large'
+        }))
+        .pipe(gulp.dest('./dist/img'))
+    });
+
+    gulp.task('copy-images-medium', function() {
+        gulp.src('./img/*')
+        //Medium
+        .pipe(imageresize({
+            width: 960,
+            upscale: false,
+            crop: false,
+            imageMagick: true
+        }))
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(rename({
+            suffix: '_medium'
+        }))
+        .pipe(gulp.dest('./dist/img'))
+    });
+
+    gulp.task('copy-images-small', function() {
+        gulp.src('./img/*')
+        //Small
+        .pipe(imageresize({
+            width: 480,
+            upscale: false,
+            crop: false,
+            imageMagick: true
+        }))
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(rename({
+            suffix: '_small'
         }))
         .pipe(gulp.dest('./dist/img'))
 })
 
 gulp.task('styles', function() {
-
     // Zwei Möglichkeiten: Alle einzelnd importieren, oder über SASS alle in ein importieren
     //gulp.src('sass/**/*.scss')
     gulp.src('./sass/main.scss')
